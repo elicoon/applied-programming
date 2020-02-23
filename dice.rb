55ce5032d31fe51eb4b3e37b6b7567d94569d3c8
elicoon <elicoon@gmail.com>
2020-02-23 22:29:01 +0000

    latest updates before i get timed out

--- dice.rb
+++ dice.rb
@@ -4,5 +4,20 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "Hello"
+    #add ruby code here...
+#   "Hello"
+
+# die1=rand(1..6)
+# die2=rand(1..6)
+
+# "<h1>Dice: Your first die is #{die1}, your second die is #{die2}.</h1> <p>Your total is #{die1+die2}.</p>"
+
+#this allows us to look at a different filetype in the folder. In this case, the file called dice/rb
+view "dice"
+
+end
+
+
+get "/tacos" do
+    "Tacos taste great"
 end
